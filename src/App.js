ca3535b6931d9230476b6c236c3e5af8f6abbe7d
Dikesh Maharjan <dcasemhrz@gmail.com>
2022-12-26 02:52:41 +1100

    Updated Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
+import React, { useState } from 'react';
 
 import Navbar from './components/Navbar';
 import TextBox from './components/TextBox';
 
 function App() {
+  const [mode, setmode] = useState('light')
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setmode('dark')
+      document.body.style.backgroundColor='#3e3b3b';
+    }
+    else{
+      setmode('light')
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-    <TextBox />
+    <TextBox mode={mode}/>
     </div>
     </>
   );
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export default function TextBox() {
-  const [text, setText] = useState("Sample text");
+export default function TextBox(props) {
+  const [text, setText] = useState("");
 
   function upclickHandler() {
-
     setText(text.toUpperCase());
-
-
   }
   function lowclickHandler() {
     setText(text.toLowerCase());
   }
+  function copyhandler() {
+    navigator.clipboard.writeText(text);
+  }
   function clearHandler() {
-    setText('');
+    setText("");
   }
 
   function changeHandler(e) {
@@ -23,17 +23,93 @@ export default function TextBox() {
   return (
     <>
       <div className="form-group">
-        <textarea className="form-control my-4" value={text} onChange={changeHandler} id="textBox" rows="8"></textarea>
-        <button className="btn btn-primary mx-2" onClick={upclickHandler}>Change to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={lowclickHandler}>Change to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={clearHandler}>Clear Text</button>
-      </div>
-      <div className="container">
-        <h4 className='my-2'>Summary : {text.split(" ").length} words and {text.length} characters <i>Read Time : {(0.008 * text.split(" ").length).toFixed(2)} minutes</i></h4>
-        <h3>Preview</h3>
-        <p>{text}</p>
+        <textarea
+          className="form-control my-4"
+          style={{
+            color: props.mode === "dark" ? "white" : "#3e3b3b",
+            background: props.mode === "dark" ? "#3e3b3b" : "white",
+          }}
+          value={text}
+          onChange={changeHandler}
+          id="textBox"
+          rows="8"
+        ></textarea>
+        <button className="btn btn-primary mx-2" onClick={upclickHandler}>
+          Change to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={lowclickHandler}>
+          Change to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={clearHandler}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={copyhandler}>
+          Copy
+        </button>
       </div>
 
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h4 className="my-2">
+          Summary : {text.split(" ").length} words and {text.length} characters{" "}
+          <i>
+            Read Time : {(0.008 * text.split(" ").length).toFixed(2)} minutes
+          </i>
+        </h4>
+
+        <div
+          className="accordion"
+          id="accordionExample"
+          style={{
+            color: props.mode === "dark" ? "white" : "#3e3b3b",
+            background: props.mode === "dark" ? "#3e3b3b" : "white",
+          }}
+        >
+          <div
+            className="accordion-item"
+            style={{
+              color: props.mode === "dark" ? "white" : "#3e3b3b",
+              background: props.mode === "dark" ? "#3e3b3b" : "white",
+            }}
+          >
+            <h2
+              className="accordion-header"
+              id="headingOne"
+              style={{
+                color: props.mode === "dark" ? "white" : "#3e3b3b",
+                background: props.mode === "dark" ? "#3e3b3b" : "white",
+              }}
+            >
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={{
+                  color: props.mode === "dark" ? "white" : "#3e3b3b",
+                  background: props.mode === "dark" ? "#3e3b3b" : "white",
+                }}
+              >
+                Preview
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse show"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <p>{text.length > 0 ? text : "No preview"}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
